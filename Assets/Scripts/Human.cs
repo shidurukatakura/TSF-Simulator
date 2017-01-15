1bce95f2822a2abfcd3ff480ff7a99026b3a3978
mitsukikaminaga <mitsukikaminaga@gmail.com>
2017-01-15 15:14:51 +0900

    本当の真実の最終調整

--- Assets/Scripts/Human.cs
+++ Assets/Scripts/Human.cs
@@ -177,7 +177,7 @@ public class Human : MonoBehaviour {
             //float probabirity = 1 / (2 + 6 * Mathf.Exp(9 - count / 2)) - 0.003034f; // シグモイド関数（ロジスティック曲線）
             //float probabirity = 1 / (2 + 6 * Mathf.Exp(-(count - 25) / 3)) - 0.003034f; // シグモイド関数（ロジスティック曲線）
             //float probabirity = 1 / (2 + 6 * Mathf.Exp(-(count - 22) / 3)) - 0.003034f; // シグモイド関数（ロジスティック曲線）
-            float probabirity = 1 / (1.5f + 6 * Mathf.Exp(-(count - 22) / 3)) - 0.003034f; // シグモイド関数（ロジスティック曲線）
+            float probabirity = 1 / (1.5f + 6 * Mathf.Exp(-(count - 22) / 3)) - 0.003039f; // シグモイド関数（ロジスティック曲線）
             count++;
             if (Random.value < probabirity)
             {
